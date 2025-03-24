75df8380b409d3c58e922cf18177e2f00202ad21
shashankm03 <shashankmahaladkar@gmail.com>
2025-03-24 11:27:20 +0530

    feat: implement sign-up endpoint and user existence check with JWT token generation

--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,42 @@
 import { Hono } from "hono";
+import { signUpWithUsernameAndPassword } from "../controllers/authentication";
+import { SignUpWithUsernameAndPasswordError } from "../controllers/authentication/+types";
 
 export const hono = new Hono();
+
+hono.post("/authentication/sign-up", async (context) => {
+  const { username, password } = await context.req.json();
+
+  try {
+    const result = await signUpWithUsernameAndPassword({
+      username,
+      password,
+    });
+
+    return context.json(
+      {
+        data: result,
+      },
+      201
+    );
+  } catch (e) {
+    if (e === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME) {
+      return context.json(
+        {
+          message: "Username already exists",
+        },
+        409
+      );
+    }
+
+    return context.json(
+      {
+        mesage: "Unknown",
+      },
+      500
+    );
+  }
+});
 
 hono.get("/health", (context) => {
   return context.json(
@@ -9,26 +45,4 @@ hono.get("/health", (context) => {
     },
     200
   );
-});
-
-hono.get("/authentication/sign-up", async (context) => {
-    const { username, password } = await context.req.json();
-    try{
-        const result = await SignUpWithUsernameAndPassword({ username, password });
-
-        return context.json(
-            {
-                data : result,
-            }, 201
-        );
-    }
-    
-    catch(e){
-        return context.json(
-            {
-                message: "Error",
-            },
-            500
-        );
-    }
 });
