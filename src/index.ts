f1f7518f605b28c9105793394dd58356844f3cef
shashankm03 <shashankmahaladkar@gmail.com>
2025-03-24 06:47:48 +0530

    initialize project with basic setup, including README, environment configuration, and JWT authentication

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+
+import jwt from "jsonwebtoken";
+import { jwtSecretKey } from "./environment";
+
+console.log(jwtSecretKey);
+
+const payload: jwt.JwtPayload = {
+  iss: "https://purpleshorts.co.in",
+  sub: "Shashank",
+};
+
+const token = jwt.sign(payload, jwtSecretKey, {
+  algorithm: "HS256",
+});
+
+console.log("Token", token);
+
+try {
+  const decodedPayload = jwt.verify(token, jwtSecretKey);
+
+  console.log("Decoded Payload", decodedPayload);
+} catch (e) {
+  console.log("Error", e);
+  ("");
+}
