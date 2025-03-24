ecd022e68ace82fb524b96084fb5c895cb7d7f6c
shashankm03 <shashankmahaladkar@gmail.com>
2025-03-24 11:27:26 +0530

    feat: refactor sign-up logic to include user existence check and JWT token generation

--- src/controllers/authentication/index.ts
+++ src/controllers/authentication/index.ts
@@ -2,21 +2,48 @@ import { createHash } from "crypto";
 
 import { SignUpWithUsernameAndPasswordError, type SignUpWithUsernameAndPasswordResult } from "./+type";
 import { prismaClient } from "../../extras/prisma";
-import { sign, type JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { jwtSecretKey } from "../../../environment";
+
+const createJWToken = (parameters: { id: string; username: string }): string => {
+  // Generate token
+  const jwtPayload: jwt.JwtPayload = {
+    iss: "https://purpleshorts.co.in",
+    sub: parameters.id,
+    username: parameters.username,
+  };
+
+  const token = jwt.sign(jwtPayload, jwtSecretKey, {
+    expiresIn: "30d",
+  });
+
+  return token;
+};
+
+export const checkIfUserExistsAlready = async (parameters: { username: string }): Promise<boolean> => {
+  const existingUser = await prismaClient.user.findUnique({
+    where: {
+      username: parameters.username,
+    },
+  });
+
+  if (existingUser) {
+    return true;
+  }
+
+  return false;
+};
 
 export const signUpWithUsernameAndPassword = async (parameters: {
   username: string;
   password: string;
 }): Promise<SignUpWithUsernameAndPasswordResult> => {
   try {
-    const existingUser = await prismaClient.user.findUnique({
-      where: {
-        username: parameters.username,
-      },
+    const isUserExistingAlready = await checkIfUserExistsAlready({
+      username: parameters.username,
     });
 
-    if (existingUser) {
+    if (isUserExistingAlready) {
       throw SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME;
     }
 
@@ -29,15 +56,9 @@ export const signUpWithUsernameAndPassword = async (parameters: {
       },
     });
 
-    // Generate token
-    const jwtPayload: JwtPayload = {
-      iss: "https://purpleshorts.co.in",
-      sub: user.id,
+    const token = createJWToken({
+      id: user.id,
       username: user.username,
-    };
-
-    const token = sign(jwtPayload, jwtSecretKey, {
-      expiresIn: "30d",
     });
 
     const result: SignUpWithUsernameAndPasswordResult = {
