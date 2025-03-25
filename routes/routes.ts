e49478a8803dec2b6aabf2e894346992cf2d94ce
shashankm03 <shashankmahaladkar@gmail.com>
2025-03-25 14:17:11 +0530

    feat: add user retrieval endpoint with JWT authentication middleware

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,9 +1,54 @@
 import { Hono } from "hono";
 import { authenticationRoutes } from "./authentication-routes";
+import { prismaClient } from "../src/extras/prisma";
+import jwt from "jsonwebtoken";
+import { jwtSecretKey } from "../environment";
 
 export const allRoutes = new Hono();
 
+allRoutes.use(async (context, next) => {
+  console.log("HTTP METHOD", context.req.method);
+  console.log("URL", context.req.url);
+  console.log("HEADERS", context.req.header());
+
+  await next();
+});
+
 allRoutes.route("/authentication", authenticationRoutes);
+
+allRoutes.get(
+  "/users",
+  async (context, next) => {
+    const token = context.req.header("token");
+
+    if (!token) {
+      return context.json(
+        {
+          message: "Missing Token",
+        },
+        401
+      );
+    }
+
+    try {
+      const verified = jwt.verify(token, jwtSecretKey);
+
+      await next();
+    } catch (e) {
+      return context.json(
+        {
+          message: "Missing Token",
+        },
+        401
+      );
+    }
+  },
+  async (context) => {
+    const users = await prismaClient.user.findMany();
+
+    return context.json(users, 200);
+  }
+);
 
 allRoutes.get("/health", (context) => {
   return context.json(
